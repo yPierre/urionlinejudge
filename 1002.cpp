8f92ef17656171a901e6db9ae4ae5bef6c847d18
yPierre <jeandias1997@gmail.com>
2018-01-14 15:05:38 -0200

    Area do Circulo

---
+++ 1002.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>
+ 
+using namespace std;
+ 
+int main() {
+    
+	double area, raio, pi = 3.14159;
+	cin >> raio;
+	area = (pi *(raio*raio));
+	cout << "A=" << fixed << setprecision(4) << area << endl;
+	
+    return 0;
+}
